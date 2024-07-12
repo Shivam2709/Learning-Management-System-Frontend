79e759ea2eb7e7064325c6e9a700c9394eae4cf2
Shivam Thakur <shivamthakur2709@gmail.com>
2024-07-12 18:01:49 +0530

    singup page..

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,36 +1,118 @@
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 import { BsPersonCircle } from "react-icons/bs";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
 import HomeLayout from "../Layouts/HomeLayout";
+import { createAccount } from "../Redux/Slices/AuthSlice";
 
 const SignUp = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const [previewImage, setPreviewImage] = useState("");
 
-    const [previewImage, setPreviewImage] = useState("");
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    avatar: "",
+  });
 
-    const [signupData, setSignupData ] = useState({
-        fullName: "",
-        email: "",
-        password: "",
-        avatar: ""
-    })
+  function handleUserInput(e) {
+    const { name, value } = e.target;
+    setSignupData({
+      ...signupData,
+      [name]: value,
+    });
+  }
 
-    function handleUserInput(e) {
-        const { name, value } = e.target;
-        setSignupData({
-            ...signupData,
-            [name]: value
-        })
+  function getImage(event) {
+    event.preventDefault();
+    // getting the image
+    const uploadedImage = event.target.files[0];
+
+    if (uploadedImage) {
+      setSignupData({
+        ...signupData,
+        avatar: uploadedImage,
+      });
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(uploadedImage);
+      fileReader.addEventListener("load", function () {
+        // console.log(this.result);
+        setPreviewImage(this.result);
+      });
     }
+  }
+
+  async function createNewAccount(event) {
+    event.preventDefault();
+    if (
+      !signupData.email ||
+      !signupData.password ||
+      !signupData.fullName ||
+      !signupData.avatar
+    ) {
+      toast.error("Please fill all the Details");
+      return;
+    }
+
+    // checking name field length
+    if (signupData.fullName.length < 5) {
+      toast.error("Name should be atleast 5 characters long");
+      return;
+    }
+
+    // email validation
+    if (
+      !signupData.email
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    ) {
+      toast.error("Invalid Email Id");
+      return;
+    }
+
+    // password validation
+    if(!signupData.password.match(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{6,20}$/)){
+        toast.error("Password should be 6 - 16 character long with atleast a number and spacial character");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("fullName", signupData.fullName);
+    formData.append("email", signupData.email);
+    formData.append("password", signupData.password);
+    if (signupData.avatar) {
+      formData.append("avatar", signupData.avatar);
+    }
+
+    // dispatch create account action
+    const response = await dispatch(createAccount(formData))
+    if(response.payload.success)
+        navigate("/");
+
+    setSignupData({
+      fullName: "",
+      email: "",
+      password: "",
+      avatar: ""
+    });
+    setPreviewImage("");
+  }
 
   return (
     <HomeLayout>
       <div className="flex items-center justify-center h-[90vh] ">
-        <form className="flex flex-col justify-center gap-3 rounded-lg p-4 text-white w-96 shadow-[0_0_10px_black]">
+        <form
+          noValidate
+          onSubmit={createNewAccount}
+          className="flex flex-col justify-center gap-3 rounded-lg p-4 text-white w-96 shadow-[0_0_10px_black]"
+        >
           <h1 className="text-center text-2xl font-bold">Registration Form</h1>
 
           <label htmlFor="image_uploads" className="cursor-pointer">
@@ -44,6 +126,7 @@ const SignUp = () => {
             )}
           </label>
           <input
+            onChange={getImage}
             className="hidden"
             type="file"
             name="image_uploads"
@@ -51,8 +134,10 @@ const SignUp = () => {
             accept=".jpg, .jpeg, .png, .svg"
           />
 
-            <div className="flex flex-col gap-1">
-            <label htmlFor="email" className="font-semibold">Full Name </label>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email" className="font-semibold">
+              Full Name{" "}
+            </label>
             <input
               type="fullName"
               id="fullName"
@@ -63,9 +148,11 @@ const SignUp = () => {
               onChange={handleUserInput}
               value={signupData.fullName}
             />
-            </div>
+          </div>
           <div className="flex flex-col gap-1">
-            <label htmlFor="email" className="font-semibold">Email</label>
+            <label htmlFor="email" className="font-semibold">
+              Email
+            </label>
             <input
               type="email"
               id="email"
@@ -78,7 +165,9 @@ const SignUp = () => {
             />
           </div>
           <div className="flex flex-col gap-1">
-            <label htmlFor="password" className="font-semibold">Password</label>
+            <label htmlFor="password" className="font-semibold">
+              Password
+            </label>
             <input
               type="password"
               id="password"
@@ -90,12 +179,18 @@ const SignUp = () => {
               value={signupData.password}
             />
           </div>
-          <button type="submit" className="mt-2 bg-yellow-600 hover:bg-yellow-500 transition-all ease-out duration-300 rounded-sm py-2 font-semibold text-lg cursor-pointer">
+          <button
+            type="submit"
+            className="mt-2 bg-yellow-600 hover:bg-yellow-500 transition-all ease-out duration-300 rounded-sm py-2 font-semibold text-lg cursor-pointer"
+          >
             Create account
           </button>
 
           <p className="text-center">
-            Already have an account ? <Link to="/login" className="link text-accent cursor-pointer">Login</Link>
+            Already have an account ?{" "}
+            <Link to="/login" className="link text-accent cursor-pointer">
+              Login
+            </Link>
           </p>
         </form>
       </div>
