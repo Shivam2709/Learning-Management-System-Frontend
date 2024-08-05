e8ee49b1c767bb120ef1c16366c95668e63906fa
Shivam Thakur <shivamthakur2709@gmail.com>
2024-08-05 18:03:41 +0530

    working on delete lecture from course

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import AuthSliceReducer from "./Slices/AuthSlice";
 import CourseSliceReducer from "./Slices/CourseSlice";
-import RazorpaySlice from "./Slices/RazorpaySlice";
+import LectureSliceReducer from "./Slices/LectureSlice";
+import RazorpaySliceReducer from "./Slices/RazorpaySlice";
 
 const store = configureStore({
   reducer: {
     auth: AuthSliceReducer,
     course: CourseSliceReducer,
-    Razorpay: RazorpaySlice,
+    Razorpay: RazorpaySliceReducer,
+    lecture: LectureSliceReducer
   },
   devTools: true,
 });
